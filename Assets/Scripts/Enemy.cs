aefbdbae060eecd6450c76d80022d5d58ffafda8
gamunex <fabiowunsch@gmail.com>
2020-12-31 03:06:18 +0100

    31.12.20

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,21 +4,45 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
     private float enemyHealth;
+    [SerializeField]
     private float movementSpeed;
+
     private int killReward;
     private int damage;
 
     private GameObject targetTile;
 
+    private void Awake()
+    {
+        Enemies.enemies.Add(gameObject);
+    }
+
     private void Start()
     {
-        
+        initializeEnemy();
     }
 
     private void initializeEnemy()
     {
         targetTile = MapGenerator.startTile;
+    }
+
+    public void takeDamage(float amount)
+    {
+        enemyHealth -= amount;
+        
+        if(enemyHealth <= 0)
+        {
+            die();
+        }
+    }
+
+    private void die()
+    {
+        Destroy(transform.gameObject);
+        Enemies.enemies.Remove(gameObject);
     }
 
     private void moveEnemy()
@@ -32,15 +56,20 @@ public class Enemy : MonoBehaviour
         {
             float distance = (transform.position - targetTile.transform.position).magnitude;
 
-            if(distance < 0.00f)
+            if(distance < 0.001f)
             {
-                int currentIndex = MapGenerator.pathTiles.IndexOf(targetTile) + 1;
+                int currentIndex = MapGenerator.pathTiles.IndexOf(targetTile);
+
+                targetTile = MapGenerator.pathTiles[currentIndex + 1];
             }
         }
     }
 
     private void Update()
     {
-        
+        checkPosition();
+        moveEnemy();
+
+        takeDamage(0);
     }
 }
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] private float range;
+    [SerializeField] private float damage;
+    [SerializeField] private float timeBetweenShots; //in seconds
+
+    private float nextTimeToShoot;
+
+    private GameObject currentTarget;
+
+    private void Start()
+    {
+        nextTimeToShoot = Time.time;
+    }
+
+    private void updateNearestEnemy()
+    {
+        GameObject currentNearestEnemy = null;
+
+        float distance = Mathf.Infinity;
+
+        foreach(GameObject enemy in Enemies.enemies)
+        {
+            if(enemy != null)
+            {
+                float _distance = (transform.position - enemy.transform.position).magnitude;
+
+                if (_distance < distance)
+                {
+                    distance = _distance;
+                    currentNearestEnemy = enemy;
+                }
+            }
+
+        }
+
+        if(distance <= range)
+        {
+            currentTarget = currentNearestEnemy;
+        }
+        else
+        {
+            currentTarget = null;
+        }
+    }
+
+    private void shoot()
+    {
+        Enemy enemyScript = currentTarget.GetComponent<Enemy>();
+
+        enemyScript.takeDamage(damage);
+
+        Debug.Log(gameObject.name + " fired a shot!");
+    }
+
+    private void Update()
+    {
+        updateNearestEnemy();
+
+        if(Time.time >= nextTimeToShoot)
+        {
+            if(currentTarget != null)
+            {
+                shoot();
+                nextTimeToShoot = Time.time + timeBetweenShots;
+            }
+        }
+    }
+}
